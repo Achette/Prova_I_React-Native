b4a925c5016ab2c3c725bf2cc25a1728acf753cd
Achette <i.achette@live.com>
2024-04-07 19:27:29 -0300

    feat: cria página para buscar personagens

---
+++ src/pages/main.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import {api} from '../services/api';
+import {
+  Avatar,
+  Card,
+  CardButton,
+  CardButtonText,
+  CardButtons,
+  CardContent,
+  CardInfos,
+  Container,
+  Form,
+  InfoText,
+  Input,
+  LabelText,
+  List,
+  Name,
+  Status,
+  SubmitButton,
+} from '../styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ActivityIndicator, Alert, Keyboard, Text, View} from 'react-native';
+
+export const Main = ({navigation}: any) => {
+  const [newCharacter, setNewCharacter] = React.useState('');
+  const [characters, setCharacters] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchCharacters = async () => {
+      const storedCharacters = await AsyncStorage.getItem('characters');
+      if (storedCharacters) {
+        setCharacters(JSON.parse(storedCharacters));
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  React.useEffect(() => {
+    const saveCharacters = async () => {
+      await AsyncStorage.setItem('characters', JSON.stringify(characters));
+    };
+
+    saveCharacters();
+  }, [characters]);
+
+  
+  const handleAddNewCharacter = React.useCallback(async () => {
+    if (!newCharacter) Alert.alert('Digite um personagem para adicionar');
+
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`/?name=${newCharacter}`);
+      const data = response.data.results[0];
+
+      if (characters.find(character => character.id === data.id)) {
+        Alert.alert('Personagem já está adicionado na lista.');
+        setIsLoading(false);
+        setNewCharacter('');
+        Keyboard.dismiss();
+        return;
+      }
+
+      const firstEpisodeResponse = await api.get(data.episode[0]);
+
+      const mappedData = {
+        id: data.id,
+        name: data.name,
+        status: data.status,
+        species: data.species,
+        image: data.image,
+        location: data.location.name,
+        episode: firstEpisodeResponse.data.name,
+        created: data.created,
+      };
+
+      setCharacters([...characters, mappedData]);
+      Keyboard.dismiss();
+    } catch (e) {
+      console.error(e);
+      Alert.alert('Algo não funcionou como deveria, tente novamente!');
+    } finally {
+      setIsLoading(false);
+      setNewCharacter('');
+    }
+  }, [newCharacter, characters]);
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar personagem"
+          value={newCharacter}
+          onChangeText={character => setNewCharacter(character)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddNewCharacter}
+        />
+        <SubmitButton loading={isLoading} onPress={handleAddNewCharacter}>
+          {isLoading ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <Icon name="add" size={20} color="#fff" />
+          )}
+        </SubmitButton>
+      </Form>
+
+      <List
+        showsVerticalScrollIndicator={false}
+        data={characters}
+        keyExtractor={character => character.id}
+        renderItem={({item}) => (
+          <Card>
+            <CardContent>
+              <Avatar source={{uri: item.image}} />
+              <CardInfos>
+                <Name>{item.name}</Name>
+                <Status>
+                  {item.status} - {item.species}
+                </Status>
+                <LabelText>Last known location</LabelText>
+                <InfoText>{item.location}</InfoText>
+                <LabelText>First seen in</LabelText>
+                <InfoText>{item.episode}</InfoText>
+              </CardInfos>
+            </CardContent>
+            <CardButtons>
+              <CardButton
+                onPress={() => {
+                  navigation.navigate('Character', {
+                    name: item.name,
+                    status: item.status,
+                    species: item.species,
+                    image: item.image,
+                    location: item.location,
+                    episode: item.episode,
+                    created: item.created,
+                  });
+                }}>
+                <CardButtonText>Mais detalhes</CardButtonText>
+              </CardButton>
+              <CardButton
+                onPress={() => {
+                  setCharacters(
+                    characters.filter(character => character.id !== item.id),
+                  );
+                }}
+                style={{backgroundColor: 'red', borderRadius: 10}}>
+                <CardButtonText>Excluir</CardButtonText>
+              </CardButton>
+            </CardButtons>
+          </Card>
+        )}
+      />
+    </Container>
+  );
+};
